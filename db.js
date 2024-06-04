04521d65a95b852fb33356aee78fb20a7c658f70
ashishchauhannn <101080857+ashishchauhannn@users.noreply.github.com>
2024-06-04 22:30:50 +0530

    shift mongodb to online server

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 // const mongoURL = 'mongodb://localhost:27017/db';
-const mongoURL = "mongodb://0.0.0.0:27017/mydb";
+// const mongoURL = "mongodb://0.0.0.0:27017/mydb";
+const mongoURL = "mongodb+srv://ashishchauhannn:redmi.65@cluster0.viqsw5z.mongodb.net/";
 mongoose.connect(mongoURL,
     {
         useNewUrlParser: true,
@@ -13,10 +14,10 @@ db.on('connected', () => {
     console.log("connected.....");
 });
 db.on('error', (err) => {
-    console.log("error", err);
+    console.log("there is an error", err);
 });
 db.on('disconnected', () => {
-    console.log("disonnected......");
+    console.log("disconnected......");
 });
 
 module.exports = db;
